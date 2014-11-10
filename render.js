e32f4340827025ac6d788ea2300ec63c5e123861
Jake Rosoman <jkroso@gmail.com>
2014-11-10 18:56:15 +1300

    use cursor.destroy

--- render.js
+++ render.js
@@ -36,30 +36,26 @@ function mainSection(todos, route) {
   return ['section#main', {hidden: todos.value.size == 0},
     ['input#toggle-all', {
       type: 'checkbox',
-      checked: todos.value.every(function(todo){ return todo.get('completed') }),
-      onClick: function toggleAll(event) {
-        todos.map(function(todo){
-          return todo.set('completed', event.target.checked)
-        })
+      checked: todos.value.every(get('completed')),
+      onClick: function toggleAll() {
+        todos.map(set('completed', !this.properties.checked))
       }
     }],
     ['ul#todo-list', todos.value.toArray().map(function(todo, index){
       if (route == 'completed' && !todo.get('completed')) return null
       if (route == 'active' && todo.get('completed')) return null
-      return todoItem(todos.get(index), index, todos)
+      return todoItem(todos.get(index))
     })]]
 }
 
-function todoItem(todo, index, todos) {
+function todoItem(todo) {
   if (todo.value.get('editing')) {
     return TextInput(todo.get('title'), {
       isfocused: true,
-      onCancel: function cancel(){
-        todo.set('editing', false)
-      },
+      onCancel: function(){ todo.set('editing', false) },
       onSubmit: function save(title){
         var title = title.trim()
-        if (title == '') todos.remove(index)
+        if (title == '') todo.destroy()
         else todo.merge({editing: false, title: title})
       }
     })
@@ -67,7 +63,7 @@ function todoItem(todo, index, todos) {
   return ['li', {class: {completed: todo.value.get('completed')}},
     [Checkbox, todo.get('completed')],
     ['label', {onDblclick: function(){ todo.set('editing', true) }}, todo.value.get('title')],
-    ['button.destroy', {onClick: function(){ todos.remove(index) }}]]
+    ['button.destroy', {onClick: function(){ todo.destroy() }}]]
 }
 
 function statsSection(todos, route) {
@@ -85,9 +81,6 @@ function statsSection(todos, route) {
     }, 'Clear completed (', String(todos.value.size - todosLeft), ')']]
 }
 
-function notCompleted(todo){ return !todo.get('completed') }
-function notEditing(todo){ return !todo.get('editing') }
-
 function link(uri, text, selected) {
   return ['li', ['a', {class: {selected: selected}, href: uri}, text]]
 }
@@ -98,3 +91,8 @@ function footer(){
     ['p', 'Written by ', ['a', {href: 'http://github.com/jkroso'}, 'Jake Rosoman']],
     ['p', 'Part of ', ['a', { href: 'http://todomvc.com' }, 'TodoMVC']]]
 }
+
+function notCompleted(todo){ return !todo.get('completed') }
+function notEditing(todo){ return !todo.get('editing') }
+function get(key){ return function(o){ return o.get(key) }}
+function set(key, value){ return function(o){ return o.set(key, value) }}
